e1f2e18f4ac7001411ab3941cebac10f86bcfebc
dogtograc185 <232096816+melissapreeves-source@users.noreply.github.com>
2026-04-29 19:13:26 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.json({ limit: '99mb' }));
 
 // In-memory storage (note: this won't persist between function invocations)
 let codeQueue = [];
